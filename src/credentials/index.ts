5b53a27847302b3af00323b57219c3ef212f9bb4
Dmitry Hmel <dima.hmelios@gmail.com>
2020-04-29 15:17:06 +0300

    add credentials for third part APIs

---
+++ src/credentials/index.ts
@@ -0,0 +1,4 @@
+const movieURL = 'http://www.omdbapi.com/';
+const movieApiKey = 'b883d8da';
+const translateURL = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
+const translateApiKey = 'trnsl.1.1.20200429T114012Z.a0e750ec5616c55a.3d0968ca78c9d814cdd94ba5f3f386ed66ea01c9';
